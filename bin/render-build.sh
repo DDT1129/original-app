2d48c0766957735ab425db33e83c7f8041c2bc5d
DDT1129 <dai1129dt@gmail.com>
2023-07-14 17:40:13 +0900

    デプロイのための編集

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,6 +6,3 @@ bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:migrate
-
-# 1 Render用の設定ファイルを追加(binフォルダに、render-build.shという名称でファイルを新規作成)
-# 2 上記1~8の内容を記述する
