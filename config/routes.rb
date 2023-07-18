74747a23c9343c518ce44523a7cf9ea47e670639
DDT1129 <dai1129dt@gmail.com>
2023-07-18 19:50:22 +0900

    【修正】Deviseのユーザールート設定
    deviseユーザー登録失敗後のルーティングエラーになるのを回避するため

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  devise_scope :user do
+    get '/users', to: 'devise/registrations#new'
+  end
+    
+
   root to: "situations#index"
   resources :situations, only: :index
 end
