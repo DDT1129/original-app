cc1c75430b36bd885ec930869e8fc48405ddbf0a
DDT1129 <dai1129dt@gmail.com>
2023-07-18 18:31:03 +0900

    userモデルのテスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, allow_blank: true, with: PASSWORD_REGEX, message: '英数字の両方を含めて半角で入力してください'
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :user do
-    nickname {Faker::Name.last_name}
-    email {Faker::Internet.free_email}
-    password {Faker::Internet.password(min_length: 6)}
+    nickname              {Faker::Name.last_name}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6, max_length:128)}
     password_confirmation {password}
   end
 end
