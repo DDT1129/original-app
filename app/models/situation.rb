06316d77c2fdb00acb46c75789ecd6fc1a5fad36
DDT1129 <dai1129dt@gmail.com>
2023-07-22 10:38:27 +0900

    タイムゾーンの変更（JST）,booleanカラムの追加

--- app/models/situation.rb
+++ app/models/situation.rb
@@ -6,6 +6,6 @@ class Situation < ApplicationRecord
   
   validates :genre_id, numericality: { other_than: 1, message: "can't be blank" } 
 
-  
+  validates :status, inclusion: { in: [true, false] }
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_08_12_053718) do
+ActiveRecord::Schema.define(version: 2023_07_19_171121) do
 
   create_table "genres", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2023_08_12_053718) do
   create_table "situations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "status", default: false, null: false
     t.string "purpose"
     t.integer "genre_id", null: false
     t.bigint "user_id", null: false
