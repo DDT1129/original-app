513315b203a1c3833241ef8e24fb17690c067aa4
DDT1129 <dai1129dt@gmail.com>
2023-08-03 17:34:24 +0900

    各ビューの修正、バリデーションの設定、

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :code,     presence: true
-  #PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
-  #validates_format_of :password, allow_blank: true, with: PASSWORD_REGEX, message: '英数字の両方を含めて半角で入力してください'
+  validates :code,     length: { minimum: 6 }, format: { with: /\A\d{6}+\z/ }
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, allow_blank: true, with: PASSWORD_REGEX, message: '英数字の両方を含めて半角で入力してください'
 
   has_many :situations, dependent: :destroy
   belongs_to :family_account, optional: true #optional: true をつけてユーザー登録時は入力を避ける
