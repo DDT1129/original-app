b7cbc05455f5c0395b4da06850b4c7f45f06c79e
DDT1129 <dai1129dt@gmail.com>
2023-08-16 16:58:48 +0900

    メモページのボタン装飾

--- app/javascript/button.js
+++ app/javascript/button.js
@@ -1,6 +1,7 @@
 window.addEventListener('load', function(){
   const pullDownButtons = document.getElementsByClassName("header-icon");
-  const buttons = document.querySelectorAll(".lists-right li");
+  const buttons = document.querySelectorAll(".lists-right li, .new-memo");
+  const memolinks = document.querySelectorAll(".memo-link");
 
   for (const pullDownButton of pullDownButtons) {
     pullDownButton.addEventListener('mouseover', function(){
@@ -17,6 +18,15 @@ window.addEventListener('load', function(){
       this.setAttribute("style", "transform: scale(1.2);");
     });
     button.addEventListener('mouseout', function() {
+      this.removeAttribute("style", "none");
+    });
+  });
+
+  memolinks.forEach(function(memolink) {
+    memolink.addEventListener('mouseover', function() {
+      this.setAttribute("style", "transform: scale(1.1);");
+    });
+    memolink.addEventListener('mouseout', function() {
       this.removeAttribute("style", "none");
     });
   });
